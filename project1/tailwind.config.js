a76c988499f64e13733e034b221f8316f716da2c
sepideh <3pd.nilforoushan@gmail.com>
2023-05-06 22:10:59 +0330

    update project1-cw17

--- project1/tailwind.config.js
+++ project1/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/*.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
