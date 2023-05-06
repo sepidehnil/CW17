a45cc38e159dc07e4a5cbe3de2e4aa331656cb34
Sepideh Nilforoushan <100242839+sepidehnil@users.noreply.github.com>
2023-05-07 00:41:20 +0330

    add project1

---
+++ project1/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+<<<<<<< HEAD
+  content: ["./public/*.html"],
+=======
+  content: ["./src/**/*.{html,js}"],
+>>>>>>> 7bd8f9e900603e8a4ba8e7b761ca86aa02c7e528
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+<<<<<<< HEAD
+}
+
+=======
+};
+>>>>>>> 7bd8f9e900603e8a4ba8e7b761ca86aa02c7e528
